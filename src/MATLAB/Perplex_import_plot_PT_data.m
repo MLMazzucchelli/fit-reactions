afef317d2943cc596b49b9dbc69af4663787c547
Mattia L. Mazzucchelli <mlmazzucchelli@gmail.com>
2023-11-09 16:29:15 +0100

    Thermolab calculations of density of solid and fluid work and match with the results of Perplex. However the process is not automatic and requires a lot of user input.

--- src/MATLAB/Perplex_import_plot_PT_data.m
+++ src/MATLAB/Perplex_import_plot_PT_data.m
@@ -1,31 +1,44 @@
+% Import data from Perplex, plot them and save the isothermal profile to a
+% Matlab file for further fitting.
+%
+% The data imported from Perplex are:
+% 1) a P-T table with: T, P, rho solid, rho fluid, id of assemblage
+% 2) an isothermal profile at a given T with increasing P with variables:
+% T, P, rho solid, rho fluid, weight percent of each non-volatile component
+% 3) an isothermal profile for water at a given T with increasing P with
+% variables: T, P, rho of water
+%
+% A table is created and saved to a .mat file for further fitting, with
+% fields: T, P, rhos, rhow, sum of weight fractions of the non-volatile components
+
 clc, clear
 Tprof           = 650;          %temperture of isothermal profile [C]
-Trange_plot     = [400, 800];
-Prange_plot     = [1 30];
+Trange_plot     = [400, 800];   %T range for the plots [C]
+Prange_plot     = [1 30];       %P range for the plots [kbar]
 %% Import data generated from Perplex
 PTfile              = readtable('atg_PT.tab', 'FileType', 'text', 'VariableNamingRule', 'preserve');
 constantTdata       = readtable('antigorite_650C.tab', 'FileType', 'text', 'VariableNamingRule', 'preserve');
 constantTwater      = readtable('water_650C.tab', 'FileType', 'text', 'VariableNamingRule', 'preserve');       %Thermo properties of H2O
-constantTdata.Properties.VariableNames  = {'T', 'P', 'rho_solid', 'rho_fluid', 'SiO2_wt', 'MgO_wt', 'fo_wt'};
-constantTwater.Properties.VariableNames = {'T_h2o', 'P_h2o', 'rho_h2o'};
+constantTdata.Properties.VariableNames  = {'T', 'P', 'rhos', 'rhof', 'SiO2_wt', 'MgO_wt', 'fo_wt'};            %This needs to be set by the user
+constantTwater.Properties.VariableNames = {'Tf', 'Pf', 'rhow'};
 % Rearrange data
-constantTdata.nonVolatile_wt = constantTdata.MgO_wt + constantTdata.SiO2_wt;   %This needs to be set by the user
+constantTdata.nonVolatile_wt = constantTdata.MgO_wt + constantTdata.SiO2_wt;                                    %This needs to be set by the user
 constantTdata = [constantTdata,constantTwater];
 
 %% Reshape data for 2D plot
 PTdata      = PTfile.Variables; %convert to array
 TK          = PTdata(:,1);
 Pbar        = PTdata(:,2);
-rho_solid   = PTdata(:,3);
-rho_h2o     = PTdata(:,4);
+rhos        = PTdata(:,3);
+rhow        = PTdata(:,4);
 assemblage  = PTdata(:,5);
 nx          = sqrt(length(PTdata(:,1))); %number of colums (the grid is square)
 
 %Reshape columns into matrices
 T           = reshape(TK, nx, nx);      
 P           = reshape(Pbar, nx, nx);    
-rho_solid   = reshape(rho_solid, nx, nx);
-rho_h2o     = reshape(rho_h2o, nx, nx);
+rhos   = reshape(rhos, nx, nx);
+rhow     = reshape(rhow, nx, nx);
 assemblage  = reshape(assemblage, nx, nx);
 
 % Convert units to C and kbar
@@ -33,32 +46,31 @@ T                       = T-273.15; %T in C
 P                       = P*1e-3; %P in kbar
 constantTdata.T         = constantTdata.T-273.15;
 constantTdata.P         = constantTdata.P*1e-3;
-constantTdata.P_h2o     = constantTwater.P_h2o*1e-3;
-constantTdata.T_h2o     = constantTwater.T_h2o-273.15;
+constantTdata.Pf     = constantTwater.Pf*1e-3;
+constantTdata.Tf     = constantTwater.Tf-273.15;
 
 %Save data to mat files
-save("data_constantT.mat","constantTdata")
+save("data_constantT_Perplex.mat","constantTdata")
 %% Find the pressure of reaction as the discontinuity in solid density
-[value,idx]         = max(abs(diff(constantTdata.rho_solid)));
+[value,idx]         = max(abs(diff(constantTdata.rhos)));
 Preaction           = constantTdata.P(idx);                               % Pressure of reaction [kbar]
 %% Plot 2D data
 figure
 subplot(2,2,1)
-contourf(T,P,rho_solid), colorbar, shading interp %plot
+contourf(T,P,rhos), colorbar, shading interp %plot
 caxis([2.6e3 3.2e3]);
 hold on
 contour(T,P,assemblage, 'r')
 hold on
 plot([Tprof,Tprof], [0, 520], '--r')
 hold on
-%plot([0, 1000], [Ptrans2 , Ptrans2 ], '--k')
 xlim(Trange_plot), ylim(Prange_plot)
 title('Density solid (kg/m3)')
 xlabel('T(C)'), ylabel('P(kbar)')
 grid on
 
 subplot(2,2,3)
-contourf(T,P,rho_h2o), colorbar, shading flat %plot
+contourf(T,P,rhow), colorbar, shading flat %plot
 hold on
 contour(T,P,assemblage, 'r')
 xlim(Trange_plot), ylim(Prange_plot)
@@ -66,13 +78,12 @@ title('Density H2O (kg/m3)')
 xlabel('T(C)'), ylabel('P(kbar)')
 
 subplot(2,2,2)
-plot(constantTdata.P, constantTdata.rho_solid, 'DisplayName', 'rho solid')
+plot(constantTdata.P, constantTdata.rhos, 'DisplayName', 'rho solid')
 hold on
-plot(constantTdata.P_h2o, constantTdata.rho_h2o, 'g', 'DisplayName', 'rho water')
+plot(constantTdata.Pf, constantTdata.rhow, 'g', 'DisplayName', 'rho water')
 hold on
-plot(constantTdata.P, constantTdata.rho_fluid, '--r','DisplayName', 'rho fluid')
+plot(constantTdata.P, constantTdata.rhof, '--r','DisplayName', 'rho fluid')
 hold on
-%plot([Ptrans2 ,Ptrans2 ], [0, 5000], '--k')
 hold on
 plot([Preaction ,Preaction], [0, 5000], '-.k')
 xlim([10, 30]), ylim([0, 4000])
@@ -92,7 +103,6 @@ plot(constantTdata.P, constantTdata.nonVolatile_wt./100, 'DisplayName', 'Non-vol
 hold on
 plot(constantTdata.P, constantTdata.fo_wt./100, 'DisplayName', 'fo wt')
 hold on
-%plot([Ptrans2 ,Ptrans2], [0, 5000], '--k', 'DisplayName', 'atg -> fo + en +H2O')
 hold on
 plot([Preaction ,Preaction], [0, 5000], '-.k')
 xlim([10, 30]), ylim([0.4, 1.05])
@@ -104,6 +114,3 @@ ylabel('X'), xlabel('P(kbar)')
 grid on
 legend
 
-
-save("data_constantT.mat","constantTdata")
-clear constantTdata
--- src/MATLAB/Termolab_calculate_density.m
+++ src/MATLAB/Termolab_calculate_density.m
@@ -1,8 +1,9 @@
-%% 
+% Calculate the desity of solid and fluid along an isothermal path with
+% Thermolab.
+% Save the data to a .mat file for further fitting.
 clc, clear
-% runname = 'antigorite'
-% load([runname '_linprog']);
-%%
+runname = 'antigorite';
+%% Calculate Gibbs energy
 Tprofile         = 650+273.15;
 Pprofile         = linspace(16,30,1000) .*1e8; %P [bar]
 % Numerics
@@ -21,39 +22,39 @@ LB  = zeros(1,size(G,1)); % do not look for alph below zero, because stable phas
 for iPT = 1:length(T2d(:))
     alph(iPT,:) = linprog(G(:,iPT),[],[],Nphs,Nsys,LB); % The Gibbs energy minimization       
 end
+[G_dP,Nphs,~]  = tl_gibbs_energy(T2d(:),P2d(:)+delP,phases);% get Gibbs energy at P+dP for derivatives
 
-[G_dP,Nphs,~]  = tl_gibbs_energy(T2d(:),P2d(:)+delP,phases);% get Gibbs energy at P+dP
-%%
-molm = molmass_fun(X);                                 % get molar mass of the components
+%% Save Thermolab calculation
+save([runname '_Thermolab_linprog']);
+%% Load Thermolab calculation
+clc, clear
+runname = 'antigorite';
+load([runname '_Thermolab_linprog']);
+%% Postprocessing
+molm     = molmass_fun(X);                                                        % get molar mass of the components
+Nphases  = Nphs([14,12,1,8],:);
+fluid_id =  strcmp(phases(pc_id),fluid);                                      % Find index of fluid
+solid_id = ~fluid_id;                                                         % Find index of solids
 
-for i = 1:length(Pprofile)
-Nphases = Nphs([14,12,1,8],:)
-% Postprocessing
-Vmol     = (G_dP(:,i)-G(:,i))/delP;                                           % Equation 54
-Mmol     = Nphases'*molm;                                         % Equation 56
-rho      = Mmol./Vmol;                                                        % Equation 57
-phim     = alph(i,:)/sum(alph(i,:));                                          % Equation 52
-phi      = phim.*Vmol'./(Vmol'*phim');                                        % Equation 53
-phiw     = phim.*Mmol'./(Mmol'*phim');                                        % Equation 55
+Mmol     = Nphases'*molm;                                                     % Equation 56
+
 Cwt      = Nphases.*repmat(molm,1,size(Nphs,2))./repmat(Mmol',size(Nphases,1),1);  % Equation 58
-fluid_id =  strcmp(phases(pc_id),fluid);                                     % Find index of fluid
-solid_id = ~fluid_id;                                                        % Find index of solids
-rhos(i)     = rho(solid_id)'*phi(solid_id)'/sum(phi(solid_id));                  % Equation 59
-rhof(i)     = rho(fluid_id)'*phi(fluid_id)'/sum(phi(fluid_id));                  % Fluid density
 
+% Calculate density variation with P
+for i = 1:length(Pprofile)
+    phim     = alph(i,:)/sum(alph(i,:));                                          % Equation 52
+    phiw     = phim.*Mmol'./(Mmol'*phim');                                        % Equation 55, weigth fraction of each phase
+    Vmol     = (G_dP(:,i)-G(:,i))/delP;                                          % Equation 54    
+    phi      = phim.*Vmol'./(Vmol'*phim');                                       % Equation 53
+    rho      = Mmol./Vmol;                                                       % Equation 57
+    rhos(i)     = rho(solid_id)'*phi(solid_id)'/sum(phi(solid_id));              % Equation 59
+    rhof(i)     = rho(fluid_id)'*phi(fluid_id)'/sum(phi(fluid_id));              % Fluid density
 end
-
-
-figure
-subplot(311)
-hold on
-plot(Pprofile, rhos, 'o','DisplayName','Thermodynamic data')
-ylabel('Density [kg/m^3]')
-xlabel('Pressure [kbar]')
-title('A) Solid density vs fluid pressure')
-set(gca,'ytick',[2000:250:3500])
-axis([Pfitting(1) Pfitting(2) 2400 3500])
-grid on
-plot([Preaction Preaction],[0 4000],'--k')
-text(17,2500,'fo + en + H2O','fontangle','italic','fontsize',10,'Rotation',90)
-text(22,3000,'atg','fontangle','italic','fontsize',10)
+rhow         = rho_H2O(Tprofile,Pprofile,'ZD05');                  % Calculate density of water
+%% Save data for fitting
+constantTdata       = table();
+constantTdata.T     = T2d'-273.15;
+constantTdata.P     = P2d'.*1e-8;
+constantTdata.rhos  = rhos';
+constantTdata.rhow  = rhow';
+save("data_constantT_Thermolab.mat","constantTdata")
